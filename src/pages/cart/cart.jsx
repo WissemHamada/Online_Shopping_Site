bb66e0be757686c7faa96ae045c5d8cfba8bd0d0
WissemHamada <wissemhamada@gmail.com>
2023-10-09 18:00:13 +0100

    first commit

---
+++ src/pages/cart/cart.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { PRODUCTS } from '../../products';
+import { ShopContext } from '../../context/shop-context';
+import { useContext } from 'react'
+import CartItem from './cart-item';
+import "./cart.css";
+import { useNavigate } from 'react-router-dom';
+
+
+export const Cart = () => {
+  const {cartItems,getTotalCartAmount } = useContext(ShopContext);
+  const totalAmount = getTotalCartAmount();
+
+  const navigate = useNavigate();
+  return (
+    <div className='cart'> 
+      <div>
+        <h1>
+          Your Cart Items
+        </h1>
+      </div>
+      <div className='cartItems'>
+        {PRODUCTS.map((product) => { 
+          if (cartItems[product.id] !== 0) {
+          return <CartItem data={product}/>
+        }}
+        
+        )}
+
+        {totalAmount >0 ?
+          <div className="checkout">
+            
+            <p> Subtotal: ${totalAmount}</p>
+            <button onClick={() => navigate('/')}>Continue Shopping </button>
+            <button> Checkout </button> 
+           
+          </div> 
+        : <h2>Your Cart is Empty</h2> }
+      </div>
+    </div>
+  )
+}
+export default Cart;
---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { PRODUCTS } from '../../products';
+import {Product} from './product';
+import './shop.css'
+
+export const Shop = () => {
+  return (
+    <div className="shop">
+        <div className='shopTitle'>
+            <h1>Shopping</h1>
+        </div>
+        <div className='products'>
+          {" "}
+            {PRODUCTS.map((product) => (
+            <Product key={Product.id} data={product}/>
+            ))}
+
+        </div>
+
+    </div>
+  )
+}
+export default Shop;
